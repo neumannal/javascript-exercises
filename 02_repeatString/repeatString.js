779877256da3d14e69251cef53f34e663dcfc254
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-11-24 23:47:18 +0100

    fix: repeat string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if (repeats >= 0) {
+        reply = ""
+        for (let i=0; i<repeats; i++) {
+            reply += string;
+        }
+        return reply;    
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
