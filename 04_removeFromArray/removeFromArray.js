f9470aa0990347920abfb948ef93e857e0575525
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-11-25 00:01:06 +0100

    fix: remove from array function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removalItems) {
+    for (item of removalItems) {
+        let idx = arr.indexOf(item);
+        while (idx !== -1) {
+            arr.splice(idx, 1);
+            idx = arr.indexOf(item);
+        }    
+    }
+    return arr;
 };
 
 // Do not edit below this line
