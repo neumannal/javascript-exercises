20ae158b15362713160fdde3e2fa5bd8200bb10f
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-11-25 07:37:50 +0100

    fix: add conversion functionality

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  const temperatureInCelsius = (temperatureInFahrenheit - 32) * 5/9;
+  return +temperatureInCelsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  const temperatureInFahrenheit = temperatureInCelsius * 9/5 + 32;
+  return +temperatureInFahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
