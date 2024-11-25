34aa92852601e8aa0ed9290112ab59b23a021be5
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-11-25 07:14:21 +0100

    fix: add sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+// Implement a function that takes 2 positive integers and returns the sum of 
+// every integer between (and including) them:
+const sumAll = function(int1, int2) {
+    if (
+        (typeof (int1+int2) !== "number") || 
+        (int1 !== Math.floor(int1)) ||
+        (int2 !== Math.floor(int2)) ||
+        (int1 < 0) ||
+        (int2 < 0)
+    ) {
+        return "ERROR";
+    }
 
+    let returnValue = 0;
+
+    let smallerInt = int1;
+    let largerInt = int2;
+    if (int1 > int2) {
+        smallerInt = int2;
+        largerInt = int1;
+    }
+
+    for (let i = smallerInt; i <= largerInt; i++) {
+        returnValue += i;
+    }
+
+    return returnValue;
 };
 
 // Do not edit below this line
