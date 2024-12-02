f7bf5c736f5fe7e3bca1a7f2e365b19f1b6111e4
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-12-02 19:46:23 +0100

    feat: add logic

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort( (a, b) => {
+        const currentYear = new Date().getFullYear();
+        let ageA = currentYear - a.yearOfBirth;
+        let ageB = currentYear - b.yearOfBirth;
+        if (a.yearOfDeath) {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (b.yearOfDeath) {
+            ageB = b.yearOfDeath - a.yearOfBirth;
+        }
+        return ageA - ageB;
+    } )
+    return people.pop();
 };
 
 // Do not edit below this line
