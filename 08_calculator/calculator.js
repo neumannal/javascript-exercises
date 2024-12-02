a66bb0d951184df58868f7c2ca2742ad42a7259c
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-12-02 17:42:55 +0100

    add logic

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const result = arr.reduce( (sum, item) => { return sum + item }, 0 );
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const result = arr.reduce( (sum, item) => { return sum * item } );
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+  let result = 1;
+	for (let index = factor; index > 0; index--) {
+    result = result * index;
+  }
+  return result;
 };
 
 // Do not edit below this line
