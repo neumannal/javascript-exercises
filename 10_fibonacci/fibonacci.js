dee3637e4a55e3d533a416663ba72a055ec9e1d7
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-12-02 19:32:34 +0100

    feat: add fibonacci logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    sequence = [1, 1]
+    if (number < 0) {
+        return "OOPS";
+    }
+    else if (number == 0) {
+        return 0;
+    } else if (number <= 2) {
+        return 1;
+    } else {
+        for (let i=2; i<number; i++) {
+            sequence.push(sequence[i-2] + sequence[i-1]);
+        }
+        return sequence.pop();
+    }
 };
 
 // Do not edit below this line
