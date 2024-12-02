3b09d49b032414023753482882b9d8aa9548e300
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-12-02 19:19:46 +0100

    feat: add palindrome logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    forward = str
+    .replaceAll(".", "")
+    .replaceAll(" ", "")
+    .replaceAll(",", "")
+    .replaceAll("!", "")
+    .toUpperCase();
+    reverse = forward
+        .split("")
+        .reverse()
+        .join("");
+    console.log(forward);
+    console.log(reverse);
+    return forward === reverse
 };
 
 // Do not edit below this line
